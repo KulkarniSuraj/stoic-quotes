d9abc336ceb63b59c6874cebbb5de9a24a4b5fa0
nb <30728951+KulkarniSuraj@users.noreply.github.com>
2023-04-16 18:38:49 +0530

    Removes unused files and creates basic stoic quote generator application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
+const URL = "https://api.themotivate365.com/stoic-quote"
 
 function App() {
+  const [quote_data, setQuote]  = useState('');
+  const fetchQuote = () => {
+    fetch(URL)
+    .then(response => response.json())
+    .then(data => setQuote(data))
+    .catch(err => console.error(err));
+  }
+useEffect(() => {
+ fetchQuote();
+}, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Press refresh button to get random stoic quote</h1>
+    <button onClick={fetchQuote}>refresh</button>
+    <div className="card">
+      <p className="quote_text">
+        "{quote_data.quote}"
+      </p>
+      <p className="author">- {quote_data.author}</p>
+    </div>
+    
     </div>
   );
 }
